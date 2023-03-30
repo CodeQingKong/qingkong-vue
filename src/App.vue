36df297c5693c5c50e601e235fd1c91c4f48ae99
luozhiwei <logan.zw.luo@cn.ey.com>
2023-03-30 13:40:13 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template xmlns="http://www.w3.org/1999/html">
+<!--  <el-text type="danger" v-show="isNameShow">姓名：{{ obj.name }}</el-text>-->
+<!--  <br>-->
+<!--  <el-text type="danger" v-show="isAgeShow">年龄：{{ obj.age }}</el-text>-->
+<!--  <br>-->
+<!--  <el-button type="success" @click="increment">count++：{{ count }}</el-button>-->
+  <hello v-for="player in array" :name=player.name :age=player.age></hello>
+</template>
+
+<script setup>
+import {ref, reactive, computed} from "vue";
+import Hello from "@/components/Hello.vue";
+
+const count = ref(0)
+const obj = reactive({
+  name: "Logan Luo",
+  age: "24"
+})
+
+const isNameShow = computed(() => {
+  return count.value >= 2
+})
+const isAgeShow = computed(() => {
+  return count.value >= 1
+})
+
+const array = [
+  {
+    name: "梅西",
+    age: 32
+  },
+  {
+    name: "C罗",
+    age: 38
+  },
+  {
+    name: "内马尔",
+    age: 36
+  }
+]
+
+function increment() {
+  count.value++;
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-button type="success">姓名：{{name}}</el-button>
+  <el-button type="warning">年龄：{{age}}</el-button>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+  age: Number
+})
+</script>
+
+<style scoped>
+
+</style>
